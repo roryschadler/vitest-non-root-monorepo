2d71eae1e836165e22bf51dbb3c86509681d3805
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2024-03-20 09:33:12 -0400

    Add side-effect to server test

--- typescript-project/server/tests/dummy.spec.ts
+++ typescript-project/server/tests/dummy.spec.ts
@@ -1,6 +1,9 @@
+import { writeFileSync } from "fs";
+
 describe("Dummy Test Suite", () => {
   it("should pass", () => {
     const result = 2 + 2;
     expect(result).to.equal(4);
+    writeFileSync("dummy-server.txt", "Hello World");
   });
 });
