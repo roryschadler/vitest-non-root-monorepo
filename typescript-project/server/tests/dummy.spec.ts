062703a8378af4cd1eb864715fa701761e689820
Rory Schadler <48921090+roryschadler@users.noreply.github.com>
2024-04-16 12:28:17 -0400

    Only run side-effect when asked

--- typescript-project/server/tests/dummy.spec.ts
+++ typescript-project/server/tests/dummy.spec.ts
@@ -4,6 +4,8 @@ describe("Dummy Test Suite", () => {
   it("should pass", () => {
     const result = 2 + 2;
     expect(result).to.equal(4);
-    writeFileSync("dummy-server.txt", "Hello World");
+    if (process.env.RUN_SIDE_EFFECT === "true") {
+      writeFileSync("dummy-server.txt", "Hello World");
+    }
   });
 });
